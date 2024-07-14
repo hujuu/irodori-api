c4c49e7ca5457dd7842bc340c99c3e7f0c49e7fe
hujuu <1961946+hujuu@users.noreply.github.com>
2024-07-15 00:00:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -46,11 +46,6 @@ async def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-
 @app.post("/api/gifts", response_description="Add new Gift")
 async def create_gift(gift: GiftBase = Body(...)):
     global mongodb
